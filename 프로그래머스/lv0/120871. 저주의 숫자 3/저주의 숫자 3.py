9c9db9a37d7394aa42e062f27e19218b1315f8a0
TakeKnowledge <starmekey@naver.com>
2023-02-08 08:21:00 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    
+    for _ in range(1, n + 1):
+        answer += 1
+        
+        while answer % 3 == 0 or str(answer).count('3') != 0:
+            answer += 1
+        
+    return answer
