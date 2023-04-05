5c1ed7d8076a33be8081a1cb3f9393b46d3b49c8
TakeKnowledge <starmekey@naver.com>
2023-04-05 09:58:16 +0900

    [Silver II] Title: 제곱수의 합, Time: 4664 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+dp = [i for i in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, i):
+        if (j ** 2) > i:
+            break
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j ** 2] + 1 
+            
+print(dp[n])
