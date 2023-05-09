11210b047c0c2680576835def2018fda9f043be1
TakeKnowledge <starmekey@naver.com>
2023-05-10 08:26:26 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
@@ -0,0 +1,10 @@
+def solution(str_list, ex):
+    answer = ''
+    
+    for string in str_list:
+        if ex in string:
+            continue
+        else:
+            answer += string
+    
+    return answer
