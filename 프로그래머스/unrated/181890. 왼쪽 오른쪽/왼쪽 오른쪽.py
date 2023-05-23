b17cd68cf240eecd7b8b6697e0b3d0942d89386a
TakeKnowledge <starmekey@naver.com>
2023-05-23 14:08:44 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,13 +1,12 @@
 def solution(str_list):
     answer = []
     
-    if 'l' in str_list or 'r' in str_list:
-        for i, c in enumerate(str_list):
-            if c == 'l':
-                answer = str_list[:i]
-                break
-            elif c == 'r':
-                answer = str_list[i + 1:]
-                break
+    for i, c in enumerate(str_list):
+        if c == 'l':
+            answer = str_list[:i]
+            break
+        elif c == 'r':
+            answer = str_list[i + 1:]
+            break
 
     return answer
