9d5efbdabc5a3ed7d83aa4230d82dbf7bfe789c3
TakeKnowledge <starmekey@naver.com>
2023-06-02 13:20:38 +0900

    [Silver III] Title: 분수 합, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.py
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -6,13 +6,10 @@ b_son, b_mom = map(int, input().split())
 answer_son = a_son * b_mom + b_son * a_mom
 answer_mom = a_mom * b_mom
 
-while True:
-  mom_son_gcd = gcd(answer_son, answer_mom)
-  if mom_son_gcd == 1:
-    break
-  else:
-    answer_son //= mom_son_gcd
-    answer_mom //= mom_son_gcd
+mom_son_gcd = gcd(answer_son, answer_mom)
+
+answer_son //= mom_son_gcd
+answer_mom //= mom_son_gcd
 
 print(answer_son, end=' ')
 print(answer_mom)
