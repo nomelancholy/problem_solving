11a7a7954b8e9e3852a27820a4369739573eec0b
TakeKnowledge <starmekey@naver.com>
2023-06-26 14:35:37 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.96 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,32 +1,3 @@
-def get_n_number(n, base):
-    convert_number = ''
-    reference = '0123456789ABCDEF'
-    
-    if n == 0:
-        return '0'
-    
-    while n:
-        q, r = divmod(n, base)
-        convert_number += reference[r]
-        n = q
-        
-    return convert_number[::-1]
-        
-
 def solution(n, t, m, p):
     answer = ''
-    full_string = ''
-    
-    target_number = 0
-    
-    while len(full_string) < t * m:
-        result = get_n_number(target_number, n)
-        full_string += str(result)
-        target_number += 1
-    
-    index = p-1
-    while len(answer) < t:
-        answer += full_string[index]
-        index += m
-        
     return answer
